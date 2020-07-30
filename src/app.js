8953d29468ba7d1cedc615b0379f055aded0bc88
pipe-code <felipea.0420@gmail.com>
2020-07-30 07:10:06 -0500

    Error 404 added

--- src/app.js
+++ src/app.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './Layout/Layout';
 import Index from './containers/Index/Index';
+import Error404 from './containers/Error404/Error404'
 
 import './app.scss';
 
 const App = (props) => {
+
+  let container = null
+  switch (pageSettings.page) {
+    case 'index':
+      container = <Index />
+      break;
+    default:
+      container = <Error404 />
+      break;
+  }
+
   return(
     <Layout>
-      {pageSettings.page = 'index' ? <Index /> : null}
+      {container}
     </Layout>
   )
 }
---
+++ src/containers/Error404/Error404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error404 = () => {
+    return (
+        <section>
+            <div className="container">
+                <p>[404] Page not found.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Error404;
