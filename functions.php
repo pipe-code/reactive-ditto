d05027d09f743f764a3cefc862a180ca216e105e
pipe-code <felipea.0420@gmail.com>
2020-10-06 07:37:11 -0500

    navigation endpoint created

--- functions.php
+++ functions.php
@@ -1,4 +1,9 @@
 <?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
 /**
  * Register Theme Scripts
  * https://developer.wordpress.org/reference/hooks/wp_enqueue_scripts/
@@ -11,64 +16,14 @@ function ditto_scripts() {
 add_action( 'wp_enqueue_scripts', 'ditto_scripts');
 
 /**
- * Register Navigation Menus
- * https://developer.wordpress.org/reference/functions/register_nav_menus/
+ * Theme Includes
  */
-function ditto_navigation_menus() {
-  $locations = array(
-    'main_menu' => __( 'Main Menu', 'text_domain' )
-  );
-  register_nav_menus( $locations );
-}
-add_action( 'init', 'ditto_navigation_menus' );
-
-/**
- * Theme support
- * https://developer.wordpress.org/reference/functions/add_theme_support/
- */
-add_theme_support( 'custom-logo' );
-
-/**
- * Login Styles
- */
-function ditto_login_styles() { ?>
-  <style type="text/css">
-    body {
-      background-color: #222 !important;
-    }
-    #login h1 a, .login h1 a {
-      display: none;
-    }
-    #login h1 img {
-      width: 100%;
-      max-width: 240px;
-      max-height: 180px;
-    }
-  </style>
-  <script type="text/javascript">
-    document.addEventListener("DOMContentLoaded", function(event) { 
-      let loginImg = document.createElement("img");
-        loginImg.src = "<?= get_template_directory_uri() ?>/src/assets/pipe-code-logo.svg";
-        loginImg.alt = "WordPress login image";
-        document.querySelector('#login h1').appendChild(loginImg);
-    });
-  </script>
-<?php }
-add_action( 'login_enqueue_scripts', 'ditto_login_styles' );
-
-/**
- * Disable Gutenberg
- */
-add_filter('use_block_editor_for_post', '__return_false', 10);
-add_filter('use_block_editor_for_post_type', '__return_false', 10);
-
-/**
- * Install latest jQuery version 3.5.1
- */
-if (!is_admin()) {
-	wp_deregister_script('jquery');
-	wp_register_script('jquery', ("https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"), false);
-	wp_enqueue_script('jquery');
+$madison_users_includes = array(
+  '/wordpress_settings.php',      // Wordpress config.
+  '/navigation.php',              // Register navigation menues and endpoints.
+);
+foreach ( $madison_users_includes as $file ) {
+  require_once __DIR__ . '/inc' . $file;
 }
 
 /**
--- src/components/Navbar/NavigationItems/NavigationItems.js
+++ src/components/Navbar/NavigationItems/NavigationItems.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Hamburger from './Hamburger/Hamburger';
 import NavigationItem from './NavigationItem/NavigationItem';
 
@@ -7,9 +7,20 @@ import styles from './NavigationItems.module.scss';
 const NavigationItems = () => {
 
     const [mainMenu, setMainMenu] = useState({
-        items: generalData.header.mainMenu,
+        items: null,
         open: false
     });
+
+    useEffect(() => {
+        fetch( _dittoURL_ + '/wp-json/navigation/main_menu')
+            .then(res => res.json())
+            .then(response => {
+                console.log(response);
+                setMainMenu(prevState => {
+                    return {...prevState, items: response}
+                });
+            })
+    }, []);
 
     const menuStyles = mainMenu.open ? [styles.MainMenu, styles.Open] : [styles.MainMenu];
 
@@ -25,7 +36,7 @@ const NavigationItems = () => {
                 <ul>
                     {mainMenu.items ? 
                         mainMenu.items.map(item => {
-                            return <NavigationItem key={item.ID} url={item.url} title={item.title} post={item.object_id}/>
+                            return <NavigationItem key={item.ID} url={item.url} title={item.title} post={item.page_id}/>
                         }) 
                         : null
                     }
